8134208bbafdc4ff4ac748d5633a449b24f6756e
laurabolwell <laurabolwell@hotmail.com>
2023-10-19 12:57:24 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -62,6 +62,58 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #check if username exists in staff, parent, or student databases
+        existing_staff = mongo.db.staff.find_one(
+            {"username": request.form.get("username").lower()})
+        existing_parent = mongo.db.parents.find_one(
+            {"username": request.form.get("username").lower()})
+        existing_student = mongo.db.students.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_staff:
+            #ensure hashed password matches user input
+            if check_password_hash(
+                existing_staff["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                #invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for('login'))
+
+        elif existing_parent:
+            #ensure hashed password matches user input
+            if check_password_hash(
+                existing_parent["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                #invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for('login'))
+        
+        elif existing_student:
+            #ensure hashed password matches user input
+                if check_password_hash(
+                    existing_student["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+                else:
+                    #invalid password match
+                    flash("Incorrect Username and/or Password")
+                    return redirect(url_for('login'))
+        
+        else:
+            #username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
