fe2cb6e9cc0eedd8ca856c0450e2c7b65fecba65
laurabolwell <laurabolwell@hotmail.com>
2023-10-20 16:41:29 +0000

    Add session user profile

--- app.py
+++ app.py
@@ -82,6 +82,16 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    #get the session user's username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
