c0d7930359de80925edd640e3be5b50cd2ba74bf
laurabolwell <laurabolwell@hotmail.com>
2023-11-15 13:04:37 +0000

    Edit my_reading_sessions function to sort by date

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def get_user_type():
 def my_reading_sessions(username):
     username = mongo.db.users.find_one({"username": session["user"]})
     if session["user"]:
-        reading_sessions = list(mongo.db.reading_sessions.find().sort("date", -1))
+        reading_sessions = list(mongo.db.reading_sessions.find().sort("date_sort", -1))
         users = list(mongo.db.users.find())
         students = list(mongo.db.students.find())
         return render_template("my_reading_sessions.html", username=username, users=users, reading_sessions=reading_sessions, students=students)
